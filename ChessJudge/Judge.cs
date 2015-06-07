5afe6232b91f941fd1a74eec434630d665748a41
Xander Harris <xanderh@hotmail.co.uk>
2015-06-07 16:53:11 -0700

    adding first iteration judge

---
+++ ChessJudge/Judge.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessJudge
+{
+    class Judge
+    {
+        public string name;
+        public int[,] boardState{ get; set; }
+        public string AIA;
+        public string AIB;
+
+        public Judge(string nom)
+        {
+            name = nom;
+        }
+
+        public bool Connect(string AIOne, string AITwo)
+        {
+            //Verify both ai respond to handshake
+            AIA = AIOne;
+            AIB = AITwo;
+            return true;
+
+            //else below.
+            //return false;
+        }
+
+        /// <summary>
+        /// Starts the game. 
+        /// X = random first/assignment.
+        /// A = AIA goes first.
+        /// B = AIB goes first.
+        /// Anything else = random.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public bool StartGame(char x = 'X')
+        {
+            boardState = new int[8,8];
+
+            if(char.ToUpper(x).Equals('A'))
+            {
+                return true;
+            }
+            else if(char.ToUpper(x).Equals('B'))
+            {
+                return true;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public int[,] SetupBoard(int[,] board, string white)
+        {
+            int[,] currentBoard = board;
+
+            if (white.ToUpper().Equals("AIA"))
+            {
+
+            }
+
+
+            return currentBoard;
+        }
+    }
+}
---
+++ ChessJudge/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessJudge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Chess Judge is a go between programs between two Chess AIs. Basically it tracks the state of the game and accepts/refuses turns.
+            
+            //Create a Judge.
+
+            //Connect Judge to AIs
+        }
+    }
+}
